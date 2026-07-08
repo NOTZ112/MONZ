849e621886434a057e8df4275335c13532e47ed4
vashu singh <lokayne@outlook.com>
2026-07-08 18:52:24 +0530

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,30 +1,30 @@
-import pymongo, os
+import motor.motor_asyncio
 from config import DB_URL, DB_NAME
 
-dbclient = pymongo.MongoClient(DB_URL)
+dbclient = motor.motor_asyncio.AsyncIOMotorClient(DB_URL)
 database = dbclient[DB_NAME]
 user_data = database['users']
 
 
 
 async def present_user(user_id : int):
-    found = user_data.find_one({'_id': user_id})
+    found = await user_data.find_one({'_id': user_id})
     return bool(found)
 
 async def add_user(user_id: int):
-    user_data.insert_one({'_id': user_id})
+    await user_data.insert_one({'_id': user_id})
     return
 
 async def full_userbase():
     user_docs = user_data.find()
     user_ids = []
-    for doc in user_docs:
+    async for doc in user_docs:
         user_ids.append(doc['_id'])
-        
+
     return user_ids
 
 async def del_user(user_id: int):
-    user_data.delete_one({'_id': user_id})
+    await user_data.delete_one({'_id': user_id})
     return
 
 
